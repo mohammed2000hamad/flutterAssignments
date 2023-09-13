8c36c257473a8c6dd6fa41152396454d8c59bfdb
mohammed hamad <best.developer.ps@gmail.com>
2023-09-13 10:46:14 +0300

    first commit

---
+++ lib/Assignment1/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assignment_1/Assignment1/HomeScreen().dart';
+
+void main() {
+  runApp( MainScreen());
+}
+
+class MainScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen() ,
+
+    );
+  }
+}
