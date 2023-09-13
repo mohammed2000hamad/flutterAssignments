2ffb1960036f12bd8fbd4274968ebe426e6985a0
mohammed2000hamad <61076185+mohammed2000hamad@users.noreply.github.com>
2023-09-13 11:00:55 +0300

    Update main.dart

--- lib/Assignment1/main.dart
+++ lib/Assignment1/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_assignment_1/Assignment1/HomeScreen().dart';
+import 'package:flutter_assignment_1/Assignment1/HomeScreen.dart';
 
 void main() {
   runApp( MainScreen());
